9351242259aadfcec029a6b2ffcf159b9f269e4a
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-05-01 12:11:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  # This initializes pigeon_list with all names
+  data.each do |attribute, specific|
+    specific.each do |detail, names|
+      if attribute == :gender
+        names.each do |name|
+          pigeon_list[name] = {}
+        end
+      end
+    end
+  end
+  # Now pigeon_list will be populated
+  data.each do |attribute, specific|
+    specific.each do |detail, names|
+      names.each do |name|
+        binding.pry
+        if pigeon_list[name][attribute].nil?
+          pigeon_list[name][attribute] = []
+        end
+        pigeon_list[name][attribute].push(detail.to_s)
+      end
+    end
+  end
+  binding.pry
+  pigeon_list
 end
